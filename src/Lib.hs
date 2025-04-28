3861b5d67a3035a3666e3a383bea6057a1fec1c6
sudo-ldasilva <205469926+sudo-ldasilva@users.noreply.github.com>
2025-04-28 12:06:58 -0300

    Modela los reportes

--- src/Lib.hs
+++ src/Lib.hs
@@ -9,3 +9,17 @@ type SuperPoderActivo = Bool
 type CantidadDeVida = Int
 
 type Personaje = (Nombre, PoderBasico, SuperPoder, SuperPoderActivo, CantidadDeVida)
+type Equipo = (Personaje, Personaje)
+
+
+obtenerVida :: Personaje -> Int
+obtenerVida (_, _, _, _, cantidadDeVida) = cantidadDeVida
+
+---
+
+atacarConPoderEspecial :: Personaje -> Equipo -> Equipo
+-- atacarConPoderEspecial personaje contrincantes = 
+
+estaEnLasUltimas :: Personaje -> Bool
+estaEnLasUltimas = (> 800) . obtenerVida
+
