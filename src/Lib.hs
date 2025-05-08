72e6b365df4d8cad6aee7a018d2a02265cf2bba7
sudo-ldasilva <205469926+sudo-ldasilva@users.noreply.github.com>
2025-05-07 22:45:19 -0300

    Reemplaza la tupla Personaje por el tipo de dato Personaje

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,65 +2,45 @@ module Lib () where
 
 import Text.Show.Functions ()
 
-type Nombre = String
-type PoderBasico = String
-type SuperPoder = String
-type SuperPoderActivo = Bool
-type CantidadDeVida = Int
+type Poder = Personaje -> Personaje
 
-type Personaje = (Nombre, PoderBasico, SuperPoder, SuperPoderActivo, CantidadDeVida)
-
-activarSuperPoder :: Personaje -> Personaje
-activarSuperPoder (nombre, poderBasico, superPoder, _, cantidadDeVida) = (nombre, poderBasico, superPoder, True, cantidadDeVida)
+data Personaje = UnPersonaje {
+  nombre :: String,
+  poderBasico :: Poder,
+  superPoder :: Poder,
+  superPoderActivo :: Bool,
+  cantidadDeVida :: Int
+}
 
 desactivarSuperPoder :: Personaje -> Personaje
-desactivarSuperPoder (nombre, poderBasico, superPoder, _, cantidadDeVida) = (nombre, poderBasico, superPoder, False, cantidadDeVida)
+desactivarSuperPoder personaje = personaje { superPoderActivo = False }
 
-obtenerVida :: Personaje -> Int
-obtenerVida (_, _, _, _, cantidadDeVida) = cantidadDeVida
-
-tieneSuperPoderActivo :: Personaje -> Bool
-tieneSuperPoderActivo (_, _, _, superPoderActivo, _) = superPoderActivo
-
-obtenerSuperPoder :: Personaje -> SuperPoder
-obtenerSuperPoder (_, _, superPoder, _, _) = superPoder
-
-obtenerPoderBasico :: Personaje -> PoderBasico
-obtenerPoderBasico (_, poderBasico, _, _, _) = poderBasico
+activarSuperPoder :: Personaje -> Personaje
+activarSuperPoder personaje = personaje { superPoderActivo = False }
 
 --
 
-quitarVida :: Int -> Personaje -> Personaje
-quitarVida aRestar (nombre, poderBasico, superPoder, superPoderActivo, cantidadDeVida)
-  | cantidadDeVida <= aRestar = (nombre, poderBasico, superPoder, superPoderActivo, 0)
-  | otherwise = (nombre, poderBasico, superPoder, superPoderActivo, cantidadDeVida-aRestar)
-
 matarPersonaje :: Personaje -> Personaje
-matarPersonaje personaje = quitarVida (obtenerVida personaje) personaje
+matarPersonaje personaje = personaje { cantidadDeVida = 0 }
 
-sumarVida :: Personaje -> Int -> Personaje
-sumarVida (nombre, poderBasico, superPoder, superPoderActivo, cantidadDeVida) aSumar = (nombre, poderBasico, superPoder, superPoderActivo, cantidadDeVida + aSumar)
+quitarVida :: Int -> Personaje -> Personaje
+quitarVida aRestar personaje
+  | cantidadDeVida personaje <= aRestar = matarPersonaje personaje
+  | otherwise = personaje { cantidadDeVida = cantidadDeVida personaje - aRestar}
 
 bolaEspinosa :: Personaje -> Personaje
 bolaEspinosa = quitarVida 1000
 
 ---
 
-esTuercaSanadora :: String -> Bool
-esTuercaSanadora =  (==) "sanadora"
-
-esTuercaDaninas :: String -> Bool
-esTuercaDaninas =  (==) "dañinas"
-
 lluviaDeTuercas :: String -> Personaje -> Personaje
-lluviaDeTuercas tipoDeTuercas personaje
-  | esTuercaSanadora tipoDeTuercas = sumarVida personaje 800
-  | esTuercaDaninas tipoDeTuercas = quitarVida ((div 2).obtenerVida $ personaje) personaje
+lluviaDeTuercas "sanadoras" personaje = personaje { cantidadDeVida = cantidadDeVida personaje + 800 }
+lluviaDeTuercas "dañina" personaje = quitarVida (cantidadDeVida personaje `div` 2) personaje
 
 ---
 
 agregarEnNombreEspinas :: Personaje -> Personaje
-agregarEnNombreEspinas (nombre, poderBasico, superPoder, superPoderActivo, cantidadDeVida) = (nombre ++ " Espinas estuvo aqui", poderBasico, superPoder, superPoderActivo, cantidadDeVida)
+agregarEnNombreEspinas personaje = personaje { nombre = nombre personaje ++ " Espinas estuvo aqui" }
 
 granadaDeEspinas :: Int -> Personaje -> Personaje
 granadaDeEspinas radio contrincante
@@ -70,31 +50,21 @@ granadaDeEspinas radio contrincante
 
 ---
 
+duplicarVida :: Personaje -> Personaje
+duplicarVida personaje = personaje { cantidadDeVida = cantidadDeVida personaje * 2}
+
 torretaCurativa :: Personaje -> Personaje
-torretaCurativa aliado = (activarSuperPoder.sumarVida aliado.(*2).obtenerVida) aliado
+torretaCurativa = activarSuperPoder.duplicarVida
 
 ---
 
-atacarConPoder :: Personaje -> String -> Personaje -> Personaje
-atacarConPoder _ "bola espinosa" atacado = bolaEspinosa atacado
-atacarConPoder atacante "lluvia de tuercas sanadoras" _ = lluviaDeTuercas "sanadoras" atacante
-atacarConPoder _ "lluvia de tuercas dañinas" atacado = lluviaDeTuercas "dañinas" atacado
-atacarConPoder _ "torreta curativa" atacado = torretaCurativa atacado
-atacarConPoder _ "granada de espinas" atacado = granadaDeEspinas 5 atacado
--- No puedo usar granada de espinas por que no se el radio ¿Cómo puedo hacer sino?
-
-atacarConPoderEspecial :: Personaje -> Personaje -> Personaje
-atacarConPoderEspecial personaje contrincante
-  | tieneSuperPoderActivo personaje = (atacarConPoder personaje (obtenerPoderBasico personaje).atacarConPoder personaje (obtenerSuperPoder personaje)) contrincante
-  | otherwise = contrincante
-
 estaEnLasUltimas :: Personaje -> Bool
-estaEnLasUltimas = (> 800) . obtenerVida
+estaEnLasUltimas = (< 800) . cantidadDeVida
 
 --
 
 espina :: Personaje
-espina = ("Espina", "bola de espinas", "granada de espinas de 5 metros de radio", True, 4800)
+espina = UnPersonaje {nombre = "Espina", poderBasico = bolaEspinosa, superPoder = granadaDeEspinas 5, superPoderActivo = True, cantidadDeVida = 4800}
 
 pamela :: Personaje
-pamela = ("Pamela", "lluvia de tuercas sanadoras", "torreta curativa", False, 9600)
+pamela = UnPersonaje {nombre="Pamela", poderBasico=lluviaDeTuercas "sanadoras", superPoder = torretaCurativa, superPoderActivo = False, cantidadDeVida = 9600}
